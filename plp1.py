413810b128fa467cfe157fd322d0307365bc6d5e
alimiyaho <ets1mam@gmail.com>
2025-08-12 17:28:59 +0100

    Add files via upload
    exercice

---
+++ plp1.py
@@ -0,0 +1,13 @@
+a= float(input("gine the number a: "))
+b= float(input("gine the number b: ")) 
+print("a+b is :")
+print( a+b)
+print("a-b is :")
+print(a-b)
+
+print("a*b is :")
+print(a*b)
+
+print("a/b is :")
+print(a/b)
+
